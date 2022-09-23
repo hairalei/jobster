d3fcbf68e93bb26cbc6f2a74f8e393f3ccc00870
Haira Lei <darhaii13@gmail.com>
2022-09-23 13:54:45 +0800

    add button for test demo in registration

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -98,6 +98,19 @@ const Register = () => {
           {isLoading ? 'Loading...' : 'Submit'}
         </button>
 
+        <button
+          type='button'
+          className='btn btn-block btn-hipster'
+          disabled={isLoading}
+          onClick={() => {
+            dispatch(
+              loginUser({ email: 'testUser@test.com', password: 'secret' })
+            );
+          }}
+        >
+          {isLoading ? 'loading...' : 'demo'}
+        </button>
+
         <p>
           {values.isMember ? 'Not a member yet?' : 'Already a member?'}
           <button
